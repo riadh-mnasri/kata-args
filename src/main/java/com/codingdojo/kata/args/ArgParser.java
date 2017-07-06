4ca99cb6b99ae65bf0528883bfefbf511fb00745
Riadh MNASRI <riadh.mnasri@gmail.com>
2017-07-06 17:50:06 +0200

    Repeat and enhance coding dojo session

--- src/main/java/com/codingdojo/kata/args/ArgParser.java
+++ src/main/java/com/codingdojo/kata/args/ArgParser.java
@@ -2,6 +2,8 @@ package com.codingdojo.kata.args;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.Maps;
+import com.google.common.primitives.Booleans;
+import com.google.common.primitives.Ints;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
@@ -12,72 +14,47 @@ public class ArgParser {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(ArgParser.class);
 
-    private static final String SCHEMA_REGEXP = "([a-z]?:[a-z\\s]+)*";
-    private static final String ARGUMENTS_REGEXP = "(-[a-z]?\\s*[a-z0-9/\\s]*)*";
-
     private Map<String, String> parsedSchema = Maps.newHashMap();
     private Map<String, String> parsedArguments = Maps.newHashMap();
     private Map<String, Object> typedArguments = Maps.newHashMap();
 
-
     public boolean validate(String schema, String arguments) {
         boolean result = false;
-        if (StringUtils.isEmpty(arguments) || StringUtils.isEmpty(schema)) {
-            return false;
+        if (StringUtils.isEmpty(schema) || !schema.matches("([a-z]?:[a-z\\s]+)*")) {
+            throw new IllegalArgumentException("Bad schema !!");
         }
-        // a:boolean b:boolean
-        if (!schema.matches(SCHEMA_REGEXP)) {
-            throw new IllegalArgumentException("Bad Schema");
-        }
-        if (!arguments.matches(ARGUMENTS_REGEXP)) {
-            throw new IllegalArgumentException("Bad Argument");
+
+        if (StringUtils.isEmpty(arguments) || !arguments.matches("(-[a-z]?\\s*[a-z0-9/\\s]*)*")) {
+            throw new IllegalArgumentException("Bad arguments !!");
         }
 
         parsedSchema = parseSchema(schema);
+
         parsedArguments = parseArguments(arguments);
 
-        for (Map.Entry<String, String> schemaToken : parsedSchema.entrySet()) {
-            Object argumentValue = getArgumentValue(schemaToken.getKey(), schemaToken.getValue());
-            typedArguments.put(schemaToken.getKey(), argumentValue);
-        }
+        typedArguments = parseTypes(parsedArguments);
+
+        result = isArgumentsMatchingSchema(parsedSchema, typedArguments);
         LOGGER.info("Typed arguments :: " + Arrays.asList(typedArguments));
-        return isArgumentsMatchingSchema(parsedSchema, typedArguments);
-    }
 
-    public boolean isArgumentsMatchingSchema(Map<String, String> parsedSchema, Map<String, Object> typedArguments) {
-        boolean result = false;
-        for (Map.Entry<String, Object> parsedArgument : typedArguments.entrySet()) {
-            result = parsedSchema.containsKey(parsedArgument.getKey()) ? true : false;
-        }
         return result;
     }
 
-
-    private Object getArgumentValue(String arg, String valueType) {
-        Object result = null;
-        LOGGER.info("getArgumentValue >>>> arg ::" + arg + " type :: " + valueType);
-        String argValue = parsedArguments.get(arg);
-        if ("boolean".equals(valueType)) {
-            if (argValue.isEmpty()) {
-                result = true;
-            } else {
-                result = Boolean.valueOf(argValue);
+    //Schema sample : "a:boolean b:integer c:string"
+    private Map<String, String> parseSchema(String schema) {
+        Map<String, String> parsedSchema = new HashMap<>();
+        List<String> splitSchemTokens = Splitter.on(" ").splitToList(schema);
+        for (int i = 0; i < splitSchemTokens.size(); i++) {
+            String schemaToken = splitSchemTokens.get(i);
+            List<String> arg = Splitter.on(":").splitToList(schemaToken);
+            if (arg.size() == 2) {
+                parsedSchema.put(arg.get(0), arg.get(1));
             }
-        } else if ("integer".equals(valueType)) {
-            result = Integer.valueOf(argValue);
-        } else if ("string".equals(valueType)) {
-            result = argValue;
-        } else {
-            throw new IllegalArgumentException("Unknown argument type");
         }
-        if (String.valueOf(result).isEmpty()) {
-            throw new IllegalArgumentException("Argument value couldn't be empty for [" + arg + "]");
-        }
-        return result;
+        return parsedSchema;
     }
 
-
-    //Arguments sample :  -l -p 8080 -d /usr/logs
+    //Arguments sample : -l -p 8080 -d /usr/logs
     private Map<String, String> parseArguments(String args) {
         Map<String, String> parsedArguments = new HashMap<>();
         List<String> tokens = Splitter.on(" ").splitToList(args);
@@ -101,19 +78,52 @@ public class ArgParser {
         return parsedArguments;
     }
 
-    //Schema sample : "a:boolean b:integer c:string"
-    private Map<String, String> parseSchema(String schema) {
-        Map<String, String> parsedSchema = new HashMap<>();
-        List<String> splitSchemaTokens = Splitter.on(" ").splitToList(schema);
-        for (int i = 0; i < splitSchemaTokens.size(); i++) {
-            String token = splitSchemaTokens.get(i);
-            List<String> arg = Splitter.on(":").splitToList(token);
-            if (arg.size() == 2) {
-                LOGGER.info("schema flag :: " + arg.get(0) + " schema type :: " + arg.get(1));
-                parsedSchema.put(arg.get(0), arg.get(1));
-            }
+    private Map<String, Object> parseTypes(Map<String, String> parsedArguments) {
+        for (Map.Entry<String, String> schemaToken : parsedSchema.entrySet()) {
+            Object argumentValue = getArgumentValue(schemaToken.getKey(), schemaToken.getValue());
+            typedArguments.put(schemaToken.getKey(), argumentValue);
         }
-        return parsedSchema;
+        return typedArguments;
+    }
+
+    private Object getArgumentValue(String arg, String valueType) {
+        Object result = null;
+        LOGGER.info("Arg ::" + arg + " type :: " + valueType);
+        try {
+            String argValue = parsedArguments.get(arg);
+            if ("boolean".equals(valueType)) {
+                if (argValue.isEmpty()) {
+                    result = true;
+                } else {
+                    result = Boolean.valueOf(argValue);
+                }
+            } else if ("integer".equals(valueType)) {
+                if (argValue.isEmpty()) {
+                    throw new IllegalArgumentException("Value should not be empty for"+ arg);
+                } else {
+                    result = Ints.tryParse(argValue);
+                }
+            } else if ("string".equals(valueType)) {
+                if (argValue.isEmpty()) {
+                    throw new IllegalArgumentException("Value should not be empty for"+ arg);
+                } else {
+                    result = argValue;
+                }
+            } else {
+                throw new IllegalArgumentException("Unknown argument type");
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Unknown argument type for [" + arg + "]");
+        }
+        return result;
+    }
+
+    public boolean isArgumentsMatchingSchema(Map<String, String> parsedSchema, Map<String, Object> typedArguments) {
+        boolean result = false;
+        for (Map.Entry<String, Object> typedArgument : typedArguments.entrySet()) {
+            result = parsedSchema.containsKey(typedArgument.getKey()) ? true : false;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
--- src/test/java/com/codingdojo/kata/args/ArgParserTest.java
+++ src/test/java/com/codingdojo/kata/args/ArgParserTest.java
@@ -15,61 +15,67 @@ public class ArgParserTest {
         argParser = new ArgParser();
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void test_with_no_argument_and_no_schema() throws Exception {
-        Assertions.assertThat(argParser.validate("", "")).isFalse();
+        Assertions.assertThat(argParser.validate("", ""));
     }
 
-    @Test
+
+    @Test(expected = IllegalArgumentException.class)
     public void test_with_one_argument_and_no_schema() throws Exception {
-        Assertions.assertThat(argParser.validate("", "-l")).isFalse();
+        Assertions.assertThat(argParser.validate("","-l")).isFalse();
     }
 
-    @Test
+
+    @Test(expected = IllegalArgumentException.class)
     public void test_with_many_arguments_and_no_schema() throws Exception {
-        Assertions.assertThat(argParser.validate("", "-l -p 8080 -d /usr/logs")).isFalse();
+        argParser.validate("", "-l -p 8080 -d /usr/logs");
     }
+
 
     @Test(expected = IllegalArgumentException.class)
     public void test_non_letter_schema() throws Exception {
         Assertions.assertThat(argParser.validate("123", "-l")).isFalse();
     }
 
+
     @Test(expected = IllegalArgumentException.class)
     public void test_invalid_argument_format() throws Exception {
-        argParser.validate("test", "####");
+        Assertions.assertThat(argParser.validate("test", "###")).isFalse();
     }
+
 
     @Test
     public void test_simple_boolean_value() throws Exception {
         Assertions.assertThat(argParser.validate("a:boolean", "-a")).isTrue();
-        Boolean arg = (Boolean) argParser.getTypedArguments().get("a");
-        Assertions.assertThat(arg).isEqualTo(true);
     }
+
 
     @Test
     public void test_multiple_boolean_arguments() throws Exception {
         Assertions.assertThat(argParser.validate("a:boolean b:boolean", "-a true -b false")).isTrue();
     }
 
+
     @Test
     public void test_simple_string_value() throws Exception {
         Assertions.assertThat(argParser.validate("s:string", "-s toto")).isTrue();
     }
 
+
     @Test
     public void test_multiple_string_value() throws Exception {
-        Assertions.assertThat(argParser.validate("s:string b:string", "-s toto -b titi")).isTrue();
+        Assertions.assertThat(argParser.validate("a:string b:string", "-a toto -b titi")).isTrue();
     }
 
     @Test
     public void test_multiple_mixed_types() throws Exception {
-        Assertions.assertThat(argParser.validate("l:boolean p:integer d:string", "-l -p 8080 -d /usr/logs")).isTrue();
+        Assertions.assertThat(argParser.validate("a:string b:integer", "-a azerty -b 343332"));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void test_missing_string_value() throws Exception {
-        Assertions.assertThat(argParser.validate("s:string", "-s"));
+        Assertions.assertThat(argParser.validate("x:string y:string", "-x toto -y"));
     }
 
     @After
