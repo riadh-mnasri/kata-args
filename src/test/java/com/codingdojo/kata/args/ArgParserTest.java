f55c304bc5c924f587ed8ada218fc48d5695109b
Riadh MNASRI <riadh.mnasri@gmail.com>
2017-07-06 18:21:52 +0200

    Clear tests for preparing coding dojo session

--- src/test/java/com/codingdojo/kata/args/ArgParserTest.java
+++ src/test/java/com/codingdojo/kata/args/ArgParserTest.java
@@ -15,67 +15,56 @@ public class ArgParserTest {
         argParser = new ArgParser();
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void test_with_no_argument_and_no_schema() throws Exception {
-        Assertions.assertThat(argParser.validate("", ""));
     }
 
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test//-l
     public void test_with_one_argument_and_no_schema() throws Exception {
-        Assertions.assertThat(argParser.validate("", "-l")).isFalse();
     }
 
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test//-l -p 8080 -d /usr/logs
     public void test_with_many_arguments_and_no_schema() throws Exception {
-        argParser.validate("", "-l -p 8080 -d /usr/logs");
     }
 
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test//123 -l
     public void test_non_letter_schema() throws Exception {
-        Assertions.assertThat(argParser.validate("123", "-l")).isFalse();
     }
 
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test//test ###
     public void test_invalid_argument_format() throws Exception {
-        Assertions.assertThat(argParser.validate("test", "###")).isFalse();
     }
 
 
-    @Test
+    @Test//a:boolean -a
     public void test_simple_boolean_value() throws Exception {
-        Assertions.assertThat(argParser.validate("a:boolean", "-a")).isTrue();
     }
 
 
-    @Test
+    @Test//a:boolean b:boolean -a true -b false
     public void test_multiple_boolean_arguments() throws Exception {
-        Assertions.assertThat(argParser.validate("a:boolean b:boolean", "-a true -b false")).isTrue();
     }
 
 
-    @Test
+    @Test//s:string -s toto
     public void test_simple_string_value() throws Exception {
-        Assertions.assertThat(argParser.validate("s:string", "-s toto")).isTrue();
     }
 
 
-    @Test
+    @Test//a:string b:string -a toto -b titi
     public void test_multiple_string_value() throws Exception {
-        Assertions.assertThat(argParser.validate("a:string b:string", "-a toto -b titi")).isTrue();
     }
 
-    @Test
+    @Test//a:string b:integer -a azerty -b 343332
     public void test_multiple_mixed_types() throws Exception {
-        Assertions.assertThat(argParser.validate("a:string b:integer", "-a azerty -b 343332"));
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test//x:string y:string -x toto -y
     public void test_missing_string_value() throws Exception {
-        Assertions.assertThat(argParser.validate("x:string y:string", "-x toto -y"));
     }
 
     @After
